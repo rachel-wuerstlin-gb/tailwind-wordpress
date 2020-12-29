c28edf8d00c8475950928637ef6896d45459f62d
Rachel Wuerstlin <rachelwuerstlin@Rachels-MBP-2.attlocal.net>
2020-12-29 14:25:15 -0800

    Adjusted Header sizing

--- wp-content/themes/tailwindtheme/header.php
+++ wp-content/themes/tailwindtheme/header.php
@@ -28,6 +28,8 @@
 
 	<header id="masthead">
 		
+		
+
 		<div class="site-branding">
 			<?php the_custom_logo();?>
 				<!-- <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1> -->
--- wp-content/themes/tailwindtheme/tailwind.js
+++ wp-content/themes/tailwindtheme/tailwind.js
@@ -6,3 +6,5 @@ module.exports = {
   variants: {},
   plugins: [],
 }
+
+
